fd305d5350eb578c45ec228bf1875e8cd76afbb8
Matvei01 <matvei.soboliv@gmail.com>
2025-05-16 17:45:19 +0300

    Bonfire

---
+++ Bonfire.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonfire : MonoBehaviour
+{
+    public float LifeTime = 15f;
+    public float HitPower = 0.1f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        LifeTime -= Time.deltaTime;
+        if(LifeTime <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.GetComponent<Temperature>()!= null)
+        {
+            Temperature temperature = other.GetComponent<Temperature>();
+            if (temperature.temperatureCurrent < temperature.temperatureCurrent)
+            {
+                temperature.temperatureCurrent += heatPower * Time.deltaTime;
+            }
+        }
+    }
+}
